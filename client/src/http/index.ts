2139b8d567db8dde3346c100c853ce768ad60d35
webdev <webdev1264@gmail.com>
2023-07-05 19:36:54 +0200

    API URL changed

--- client/src/http/index.ts
+++ client/src/http/index.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_URL = "http://localhost:5000/api";
+export const API_URL = "https://cats-wiki-api.vercel.app/api";
 
 const api = axios.create({
   baseURL: API_URL,
